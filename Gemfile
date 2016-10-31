81d35f7ea43546022040003bab95b9ff065a1062
zenglue <keonilrath@gmail.com>
2016-10-31 01:36:22 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -2,15 +2,19 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'activerecord', '4.2.5'
+gem 'sinatra-activerecord'
+gem 'rake'
 gem 'thin'
 gem 'require_all'
 
-
 group :development do
-	gem 'shotgun'
-	gem 'pry'
+		gem 'shotgun'
+		gem 'pry'
+		gem 'tux'
+		gem 'sqlite3'
 end
-    
+
 group :test do
   gem 'rspec'
   gem 'capybara'
---
+++ db/migrate/20161031052931_create_dogs.rb
@@ -0,0 +1,20 @@
+class CreateDogs < ActiveRecord::Migration
+#   def up
+#     create_table :dogs do |t|
+#       t.string :name
+#       t.string :breed
+#     end
+#   end
+#
+#   def down
+#     drop_table :dogs
+#   end
+# end
+# refactored
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+end
